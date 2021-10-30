8bd8bc846dd6df04b7426a7c708e38ba05a8f150
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2021-10-30 00:08:37 -0300

    my first java program

---
+++ Eco.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+
+public class Eco {
+	public static void main(String[] args) {
+		for (int i=0; i < args.length; i++)
+			System.out.print(args[i] + " ");
+		System.out.println();
+	}
+}
+
